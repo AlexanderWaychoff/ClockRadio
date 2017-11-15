9964d8ba561517e43cfe4e5247f0af2eab29926a
Alex Waychoff <AlexWaychoff@gmail.com>
2017-11-15 14:01:59 -0600

    First working c# code I did on my own. Hurray.

---
+++ ClockRadio/ClockRadio/ClockRadio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class ClockRadio
+    {
+        public static bool isOn = false;
+        public static double station = 90.00;
+        internal static string time = "10:";
+        public static int minutes = 0;
+
+        public static double ChangeStation()
+        {
+            minutes += 1;
+            return station += 0.75;
+        }
+        public static bool TurnRadioOn()
+        {
+            return isOn = true;
+        }
+    }
+}
---
+++ ClockRadio/ClockRadio/GetInput.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class GetInput
+    {
+        public static string yesNo;
+        public static void ConsiderRadioOnOff()
+        {
+            Console.WriteLine("Would you like to turn the radio on? Put your input as lower case.");
+            yesNo = Console.ReadLine();
+            if (yesNo == "yes")
+            {
+                ClockRadio.TurnRadioOn();
+            }
+            else
+            {
+                Console.WriteLine("Very good then.");
+                Console.ReadKey();
+            }
+            if (ClockRadio.isOn)
+            {
+                Console.WriteLine("The current station is " + ClockRadio.station + " and the time is " + ClockRadio.time + ClockRadio.minutes);
+                Console.WriteLine("Would you like to change the station?  Put your input as lower case.");
+                yesNo = Console.ReadLine();
+                if (yesNo == "yes")
+                {
+                    ClockRadio.ChangeStation();
+                    ConsiderRadioOnOff();
+
+                }
+                else
+                {
+                    Console.WriteLine("Very good then.");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
